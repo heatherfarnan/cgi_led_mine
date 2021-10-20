c51199d40ee5cb41d0de98059317343823e7ef07
heatherfarnan <91498628+heatherfarnan@users.noreply.github.com>
2021-10-20 15:43:29 +0000

    added buttons and a datab to led-pwm_new.py

--- led-pwm_new.py
+++ led-pwm_new.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python37all
 import cgi
-data = cgi.FieldStorage()
-s1 = data.getvalue('slider1')
+datas = cgi.FieldStorage()
+s1 = datas.getvalue('slider1')
 
 with open('led-pwm.txt', 'w') as f:  
   f.write(str(s1))
 
+datab = cgi.FieldStorage()
+b1 = datab.getvalue('option')
+
 print('Content-type: text/html\n\n')
 print('<html>')
 print('<form action="/cgi-bin/led-pwm_new.py" method="POST">')
+
+print('  <input type="radio" name="option" value="a"> LED 1 <br>')
+print('  <input type="radio" name="option" value="b"> LED 2 <br>')
+print('  <input type="radio" name="option" value="c"> LED 3 <br>')
+print('  <input type="submit" value="Submit"> <br><br>')
+
 print('<input type="range" name="slider1" min="0" max="100" value="%s"><br>' % s1)
 print('<input type="submit" value="Change LED brightness">')
 print('</form>')
